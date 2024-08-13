e017abea4f751b468ae6690f34b842e10c930c77
CARDONA LUIS <kikecar97@gmail.com>
2024-08-13 14:35:15 -0600

    created project

---
+++ pulhapanzak-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pulhapanzak-app',
+  webDir: 'www'
+};
+
+export default config;
