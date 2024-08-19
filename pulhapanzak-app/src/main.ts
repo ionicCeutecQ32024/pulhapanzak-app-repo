4076b4526feeac588242b33f61351cfed77e7d00
CARDONA LUIS <kikecar97@gmail.com>
2024-08-19 13:57:35 -0600

    firebase created

--- pulhapanzak-app/src/main.ts
+++ pulhapanzak-app/src/main.ts
@@ -4,11 +4,16 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), provideFirebaseApp(() => initializeApp({"projectId":"gallery-app-27b2f","appId":"1:630140731879:web:cffd60edc5ce36abe2bd42","storageBucket":"gallery-app-27b2f.appspot.com","locationId":"us-central","apiKey":"AIzaSyCOUBfHZDHyU-ATG2ay5TzvRKan-_WLmog","authDomain":"gallery-app-27b2f.firebaseapp.com","messagingSenderId":"630140731879"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()),
   ],
 });
