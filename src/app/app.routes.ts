1817df8f7152327c2dcc66058e7c5f16e1563e9f
CARDONA LUIS <kikecar97@gmail.com>
2024-09-06 15:12:47 -0600

    guards created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,31 @@
+import { inject } from '@angular/core';
 import { Routes } from '@angular/router';
+import { AuthGuard } from './shared/guards/auth-guards';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
   {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full',
   },
+
   {
     path: 'register',
     loadComponent: () => import('./auth/pages/register/register.page').then( m => m.RegisterPage)
   },
+  
   {
     path: 'login',
     loadComponent: () => import('./auth/pages/login/login.page').then( m => m.LoginPage)
-  },  {
+  },
+  
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    canActivate:[()=> inject(AuthGuard).canActive()]
+  },
+  
+  {
     path: 'tabs',
     loadComponent: () => import('./tabs/tabs.page').then( m => m.TabsPage)
   },
